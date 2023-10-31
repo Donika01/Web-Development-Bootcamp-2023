f33ae81052b2ea7e9eaeb24f75ffbdbc2bc2d879
Donika Mjeku <doniikamjeku@gmail.com>
2023-10-31 23:04:17 +0100

    Running JavaScript From A Script!

---
+++ JavaScript/JavaScript_Console/Running_Js_Script/app.js
@@ -0,0 +1,4 @@
+console.log("HELLO FROM OUR FIRST JS FILE!!!");
+let total = 1 + 3;
+console.log("GOODBYE!!!");
+// We add the script at the bottom of the <body> because we want the html elements to be acknowledged by the browser before our script
