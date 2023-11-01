6a8a5cbd1098782c13245b13e79fffca346dfcb6
Donika Mjeku <doniikamjeku@gmail.com>
2023-11-01 17:56:23 +0100

    Else-If

--- JavaScript/JavaScript_Conditionals/app.js
+++ JavaScript/JavaScript_Conditionals/app.js
@@ -1,3 +1,5 @@
+// If Statements
+// Only runs code if given conditional is true
 
 if (1 + 1 === 2) {
     console.log('MATH STILL WORKS!')
@@ -12,4 +14,29 @@ if (random < 0.5) {
 if (random >= 0.5) {
     console.log('Your number is greater then 0.5!')
     console.log(random);
+}
+
+// Else If
+// If not this thing, maybe this other thing!?
+
+const dayOfWeek = 'Friday';
+
+if (dayOfWeek === 'Monday') {
+    console.log('UGHH I HATE MONDAYS')
+} else if (dayOfWeek === 'Saturday') {
+    console.log("YAY I LOVE SATURDAYS")
+} else if (dayOfWeek === 'Friday') {
+    console.log('FRIDAYS ARE DECENT. ESPECIALLY AFTER WORK')
+}
+
+const age = 30;
+
+if (age < 5) {
+    console.log("You are a baby. You get in for free")
+} else if (age < 10) {
+    console.log("You are a child. You pay $10")
+} else if (age < 65) {
+    console.log("You are an adult. You pay $20")
+} else if (age > 65) {
+    console.log("You are a senior. You pay $10")
 }
