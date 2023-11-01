b96515a811fa2e4105bb1e6100cba0691c497305
Donika Mjeku <doniikamjeku@gmail.com>
2023-11-01 17:34:44 +0100

    If Statements

---
+++ JavaScript/JavaScript_Conditionals/app.js
@@ -0,0 +1,15 @@
+
+if (1 + 1 === 2) {
+    console.log('MATH STILL WORKS!')
+}
+
+let random = Math.random();
+if (random < 0.5) {
+    console.log('Your number is less then 0.5')
+    console.log(random);
+}
+
+if (random >= 0.5) {
+    console.log('Your number is greater then 0.5!')
+    console.log(random);
+}
