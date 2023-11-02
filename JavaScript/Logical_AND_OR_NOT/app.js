1dc277522a136fa7a4bc5dcf5fa4e25196fcc30f
Donika Mjeku <doniikamjeku@gmail.com>
2023-11-02 10:22:40 +0100

    Truth-y & False-y Values

---
+++ JavaScript/Logical_AND_OR_NOT/app.js
@@ -0,0 +1,19 @@
+// Truthy and Falsy Values
+// All JS values have an inherited truthyness or falsyness about them 
+// Falsy values: false, 0, "", null, undefined, NaN
+// Truthy values: Everything else
+
+// const userInput = prompt("Enter something");
+
+// if (userInput) {
+//     console.log("TRUTHY!")
+// } else {
+//     console.log("FALSY!")
+// }
+
+if (0) {
+    console.log("TRUTHY!")
+} else {
+    console.log("FALSY!")
+}
+// < FALSY!
