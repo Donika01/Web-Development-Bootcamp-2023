e2f1d50f9ea0d4a3c1b3fbafe4dc0f300db6421e
Donika Mjeku <doniikamjeku@gmail.com>
2023-11-02 14:18:49 +0100

    Push & Pop

--- JavaScript/Arrays/app.js
+++ JavaScript/Arrays/app.js
@@ -14,5 +14,26 @@ colors[0] = 'red';
 
 colors[10] = 'purple';  // emppty x 7. The length of the array 11
 
+// Push & Pop - Add and Remove to/from the end
+// Last In First Out (LIFO)
+
+let movieLine = ['Daki', 'Arta'];
+
+movieLine.push('Pia');
+movieLine.push('Koni', 'Mami', 'Babi');
+
+movieLine.pop();
+let person = movieLine.pop(); // Mami
+
+let barbell = [];
+
+barbell.push(45);
+barbell.push(45);
+barbell.push(25);
+barbell.push(10);
+
+barbell.pop();
+
+
 
 
