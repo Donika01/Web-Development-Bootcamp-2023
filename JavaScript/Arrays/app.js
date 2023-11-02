9dbdbbdc27203c78e355d1fe1c6de32dbae3947b
Donika Mjeku <doniikamjeku@gmail.com>
2023-11-02 14:05:49 +0100

    Arrays  - Basics

---
+++ JavaScript/Arrays/app.js
@@ -0,0 +1,18 @@
+// Arrays - Data Structure that allows us to group data together
+const days = ['Monday', 'Tuesday', 'Wednesday'];
+// Array Random Access
+days[0];
+days.length
+
+days[1][0] // T - First character of the second element 
+
+// Updating arrays
+
+const colors = ['rad', 'blue', 'green'];
+
+colors[0] = 'red';
+
+colors[10] = 'purple';  // emppty x 7. The length of the array 11
+
+
+
