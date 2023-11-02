a8724f59138094da6ad8075a4987c5358b3b036f
Donika Mjeku <doniikamjeku@gmail.com>
2023-11-02 17:49:20 +0100

    Accessing Data Out Of Objects

--- JavaScript/Object_Literals/app.js
+++ JavaScript/Object_Literals/app.js
@@ -13,3 +13,24 @@ const differenThings = {
     colors: ['red', 'black']
 }
 
+// Accessing Data Out Of Objects
+
+person['lastName'] // < Swift
+// Another way 
+person.firstName // <Taylor
+
+// VALID KEYS - All keys are converted to strings (except for symbols)
+const toStrings = {true: 'dat', null: 'lalala'}
+
+// Coding Exercise 30
+const restaurant = {
+    name: 'Ichiran Ramen',
+    address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
+    city: 'Brooklyn',
+    state: 'NY',
+    zipcode: '11206',
+}
+
+//YOUR CODE GOES DOWN HERE:
+const fullAddress = `${restaurant.address}, ${restaurant.city}, ${restaurant.state} ${restaurant.zipcode}`
+
