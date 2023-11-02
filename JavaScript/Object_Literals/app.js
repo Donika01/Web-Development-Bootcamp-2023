a71d8e57a5051914646717654de7b93a95db0067
Donika Mjeku <doniikamjeku@gmail.com>
2023-11-02 17:24:48 +0100

    Creating Object Literals

---
+++ JavaScript/Object_Literals/app.js
@@ -0,0 +1,15 @@
+// Object Literals are collections of properties
+// Property = key + value 
+// Rather than accessing data using an index, we use costum keys
+
+const person = {
+    firstName: 'Taylor',
+    lastName: 'Swift'
+}
+
+const differenThings = {
+    favNum: 13,
+    isFunny: 'true',
+    colors: ['red', 'black']
+}
+
