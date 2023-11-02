a67363394f193e14852d4926e7bbc52f6b52320c
Donika Mjeku <doniikamjeku@gmail.com>
2023-11-02 12:33:00 +0100

    Logical OR

--- JavaScript/Logical_Operators/app.js
+++ JavaScript/Logical_Operators/app.js
@@ -1,9 +1,31 @@
 // Logical AND
 // Combining two pieces of logic into a single statement
 
-const password = prompt("Enter your password")
-if (password.length >= 6 && password.indexOf(' ') === -1) {
-    console.log("VALID PASSWORD!")
+// const password = prompt("Enter your password")
+// if (password.length >= 6 && password.indexOf(' ') === -1) {
+//     console.log("VALID PASSWORD!")
+// } else {
+//     console.log("INCORRECT FORMAT FOR PASSWORD!")
+// }
+
+// Logical OR
+// If one side is ture, the entire thing is true
+
+// 0-5 free
+// 5-10 $10
+// 10-65 $20
+//65+ free
+
+// The AND Operator runs before the OR Operator (Optionla to use ())
+
+const age = prompt("Enter age");
+
+if (age >= 0 && age < 5 || age >= 65) {
+    console.log("FREE")
+} else if (age >= 5 && age < 10) {
+    console.log("$10")
+} else if (age >= 10 && age < 65) {
+    console.log("$20")
 } else {
-    console.log("INCORRECT FORMAT FOR PASSWORD!")
+    console.log("INVALID AGE")
 }
