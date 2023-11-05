a89cff22386255f1cda9aef0e81eea03cc96eca6
Donika Mjeku <doniikamjeku@gmail.com>
2023-11-05 22:44:13 +0100

    Our First Function

---
+++ JavaScript/Functions/app.js
@@ -0,0 +1,19 @@
+// Functions - Reusable procedures
+// We define a chunk of code that we can then execute at a later point
+// 2 step procedure 
+// 1.Define - registering it (telling JS about it)
+// 2.Run - Execute the function
+// function() {
+//do something
+// }
+
+function singSong() {
+    console.log("DO");
+    console.log("RE");
+    console.log("MI");
+}
+
+singSong();
+singSong();
+singSong();
+singSong();
